9bbb61a8992a868148d9df3664d2aa12e69bd67b
Eric Blount <eblount@gmail.com>
2015-06-13 23:44:15 -0500

    Initial commit, with bootstrap/jquery/elixir and laravel debugbar/ide_helper

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+var paths = {
+    jquery: 'vendor/bower_components/jquery/',
+    bootstrap: 'vendor/bower_components/bootstrap-sass-official/assets/'
+}
+
+elixir(function(mix) {
+    mix.sass('style.scss', 'public/css/')
+        .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+        .scripts([
+            paths.jquery + 'dist/jquery.js',
+            paths.bootstrap + 'javascripts/bootstrap.js'
+        ], 'public/js/app.js', './')
+        .version(["css/app.css", "js/app.js"]);
+});
---
+++ resources/views/status.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Dev Status</div>
+            </div>
+        </div>
+        <script src="{{ elixir('js/app.js') }}"></script>
+    </body>
+</html>
